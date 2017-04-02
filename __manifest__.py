b377eabb91f47d18f49232045134d306addf1f91
camilozuluaga <juancamilozuluagaserna@hotmail.com>
2017-04-02 16:17:42 -0500

    agregando estructura base para trabajar en hoteleria

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Colombia - Hoteleria',
+    'category': 'Localization',
+    'version': '1.0.0',
+    'author': 'DREAMSOFT',
+    'license': 'AGPL-3',
+    'maintainer': '',
+    'website': '',
+    'summary': 'Colombia Hoteleria: Extended Hotel Management / '
+               'Contact Module - Odoo 10.0',
+    'images': [],
+    'depends': ['hotel'],
+    'data': [
+
+    ],
+    'qweb': [],
+    'installable': True,
+}
